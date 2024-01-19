dce515edb9cbc8db73604266b07d9da25f19a12b
Minnure <gminnure@hotmail.com>
2024-01-19 14:24:54 +0300

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);  // todos tüm lisetenin elemanlarını tutan settodos değişimi tutan 
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    getLocalTodos();
+  }, []) 
+  
+  // bir kere
+  
+  useEffect(() => {
+    filterHandler(todos) //ya da filterHandler ı useEffect içinde yazarsak çözülür
+    saveLocalTodos();
+  }, [todos, status]) //eslint-disable-line
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
+  //! save to local 
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const getLocalTodos = () => {
+    if(localStorage.getItem("todos") ===null) {
+      localStorage.setItem("todos", JSON.stringify([])) // boşsa bir şeyler ekleyeceğiz 
+    } else{
+      setTodos(JSON.parse(localStorage.getItem("todos")))
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <h1>My To Do List</h1>
+        </header>
+        <Form
+        inputText = {inputText}
+        setInputText = {setInputText}
+        todos = {todos}
+        setTodos ={setTodos}
+        setStatus ={setStatus}
+        />
+        <TodoList
+        todos = {todos}
+        setTodos = {setTodos} //props olarak todoliste gönderdik
+        filteredTodos = {filteredTodos}
+         />
+        
     </div>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Todo from "./Todo";
+
+function TodoList ({todos, setTodos, filteredTodos}) { //Gönderileni karşıla
+
+    return(
+      <div>
+        <div className="todo-container">
+            <ul className="todo-list">
+                {filteredTodos.map((todo) => (
+                    <Todo
+                    text = {todo.text}
+                    key = {todo.id}
+                    todo = {todo}
+                    todos = {todos}
+                    setTodos={setTodos}
+                     />))}
+
+            
+        </ul>
+        </div>
+        
+      </div>
+    )
+}
+
+export default TodoList
